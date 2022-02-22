b80e3bf921768c73c03064c6a0bbf20f2a73f904
Keira Hosey <78433426+keira-h@users.noreply.github.com>
2022-02-22 14:12:57 +0000

    pasted previous code

--- Sketch.java
+++ Sketch.java
@@ -25,12 +25,51 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+	// Drawing grass 
+    fill(163, 230, 115);
+    rect(-5, 445, 510, 75);
+  
+  // Drawing pathway
+    fill(189, 189, 189);
+    quad(400, 500, 385, 460, 295, 460, 280, 500);
+  
+  // Drawing house base
+    fill(242, 234, 203);
+    rect(80, 220, 350, 240); 
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // Drawing window base
+    fill(0, 0, 0);
+    rect(125, 310, 75, 65); 
+
+  // Drawing window glass
+    fill(255, 255, 255);
+    rect(130, 315, 30, 25);
+
+    fill(255, 255, 255);
+    rect(165, 315, 30, 25);
+
+    fill(255, 255, 255);
+    rect(130, 345, 30, 25);
+
+    fill(255, 255, 255);
+    rect(165, 345, 30, 25);  
+  
+  // Drawing door 
+    fill(156, 98, 17);
+    rect(295, 300, 90, 160);
+
+  // Drawing door knob 
+    fill(228, 235, 33);
+    ellipse(310, 395, 20, 20);
+
+  // Drawing chimney
+    fill(156, 98, 17);
+    rect(105, 65, 75, 145);
+  
+  // Drawing roof
+    fill(156, 98, 17);
+    triangle(55, 235, 255, 60, 455, 235);  
   }
   
-  // define other methods down here.
 }
