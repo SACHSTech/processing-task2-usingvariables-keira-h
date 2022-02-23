b1a342602acb49376c84d94a08170f1d7ee03303
Keira Hosey <78433426+keira-h@users.noreply.github.com>
2022-02-23 01:09:54 +0000

    part 1

--- Sketch.java
+++ Sketch.java
@@ -23,12 +23,10 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
     
 	// Drawing grass 
     fill(163, 230, 115);
-    rect(-5, 445, 510, 75);
+    rect(width/-100, height/(float)1.12359550562, width*(float)1.02, height/(float)(6.66666666667));
   
   // Drawing pathway
     fill(189, 189, 189);
@@ -36,36 +34,36 @@ public class Sketch extends PApplet {
   
   // Drawing house base
     fill(242, 234, 203);
-    rect(80, 220, 350, 240); 
+    rect(width/(float)6.25, height/(float)2.27272727273, width/(float)1.42857142857, height/(float)2.08333333333); 
 
   // Drawing window base
     fill(0, 0, 0);
-    rect(125, 310, 75, 65); 
+    rect(width/4, height/(float)1.61290322581, width/(float)6.66666666667, height/(float)7.69230769231); 
 
   // Drawing window glass
     fill(255, 255, 255);
-    rect(130, 315, 30, 25);
+    rect(width/(float)3.84615384615, height/(float)1.5873015873, width/(float)16.6666666667, height/20);
 
     fill(255, 255, 255);
-    rect(165, 315, 30, 25);
+    rect(width/(float)3.0303030303, height/(float)1.5873015873, width/(float)16.6666666667, height/20);
 
     fill(255, 255, 255);
-    rect(130, 345, 30, 25);
+    rect(width/(float)3.84615384615, height/(float)1.44927536232, width/(float)16.6666666667, height/20);
 
     fill(255, 255, 255);
-    rect(165, 345, 30, 25);  
+    rect(width/(float)3.0303030303, height/(float)1.44927536232, width/(float)16.6666666667, height/20);  
   
   // Drawing door 
     fill(156, 98, 17);
-    rect(295, 300, 90, 160);
+    rect(295, 300, 90, height/(float)3.125);
 
   // Drawing door knob 
     fill(228, 235, 33);
-    ellipse(310, 395, 20, 20);
+    ellipse(310, 395, width/25, height/25);
 
   // Drawing chimney
     fill(156, 98, 17);
-    rect(105, 65, 75, 145);
+    rect(105, height/(float)7.69230769231, width/(float)(6.66666666667), 145);
   
   // Drawing roof
     fill(156, 98, 17);
